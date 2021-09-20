92baf6269113abe21daf60e267eb23712cb76acb
Edzl <edizonlayug@gmail.com>
2021-09-21 02:58:43 +0800

    Added some routing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,33 @@
-function wee(display){
-    console.log(display)
+import path from 'path'
+import config from '../config'
+import express from 'express'
+import routes from './routes'
+import compress from 'compression'
+import cookieSession from 'cookie-session'
+import cors from 'cors'
+import helmet from 'helmet'
+import logger from 'winston'
+
+const configuration = async ( req, res, next ) => {
+	req.config = config
+	req.activity = activity
+	req.currentUrl = config.env == 'production' ? config.host: `${config.host}:${config.port}`
+	req.frontEndUrl = config.feDomain
+	req.rootDir = path.join( __dirname,'/' )
+	next()
 }
-wee("====================================================>")
+
+const app = express()
+
+app.use( cookieSession({name: 'session',	keys: [config.secret]}) )
+app.use( '/files', express.static( './src/public/files' ) )
+app.use( cors() )
+app.use(configuration)
+app.use( helmet() )
+app.use( compress() )
+app.use( express.json() )
+app.use( express.urlencoded({extended: true}) )
+app.use( routes )
+//app.use( express.errorHandler({logger}) )
+
+export default app
--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-console.log("===================================================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+import 'app-module-path/register'
+import logger from 'winston'
+import app from './app'
+import config from '../config'
+
+const serverUp = ( app ) => {
+	const server = app.listen( config.port )
+	process.on( 'unhandledRejection', ( reason, p ) =>
+		logger.error( 'Unhandled Rejection at: Promise ', p, reason )
+	)
+	server.on( 'listening', () =>{
+		logger.info( 'ResMan Application started on http://%s:%d', config.host,  config.port )
+        console.log(config.host)}
+	)
+}
+serverUp( app )
